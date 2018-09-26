f4f22190a3d413096cf5a0724cde4f9fe11415f9
Tom Fredian <twf@psfc.mit.edu>
2018-09-26 09:30:56 -0400

    New release 1.5

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ if sys.version_info < (2, 5):
     sys.exit("requires python 2.5 and up")
 
 here = os.path.dirname(__file__)
-version_string='1.4'
+version_string='1.5'
 
 setup(name = "mdsconnector",
     version = version_string, 
