623294833d4396582c96f91bc0283c22f292ca19
Josh Stillerman <jas@psfc.mit.edu>
2022-12-16 14:12:11 -0500

    Change packaging to use twine

--- src/setup.py
+++ src/setup.py
@@ -1,54 +1,5 @@
-#!/usr/bin/env python
-import sys
-import os
+# setup.py
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup
 
-if sys.version_info < (2, 5):
-    sys.exit("requires python 2.5 and up")
-
-here = os.path.dirname(__file__)
-version_string='1.8'
-
-setup(name = "mdsconnector",
-    version = version_string, 
-    description = "Remote call to MDSplus objects",
-    author = "MDSplus Core Development Team",
-    author_email = "mdsplus_dev@psfc.mit.edu",
-    maintainer_email = "mdsplus_dev@psfc.mit.edu",
-    license = "MIT",
-    url = "http://www.mdsplus.org",
-    package_dir = {'mdsconnector':'.',
-    },
-    packages = ['mdsconnector',
-    ],
-    scripts = [
-    ],
-    install_requires = ["rpyc==4.1.5", "dill<=0.3.2", "plumbum<=1.6.9"],
-    platforms = ["POSIX", "Windows"],
-    use_2to3 = False,
-    zip_safe = False,
-    long_description = """
-The mdsConnector class enables the connection to a remote system via
-SSH to utilize the MDSplus python objects remotely on that system.""",
-    classifiers = [
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
-        "Intended Audience :: System Administrators",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Topic :: Internet",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Topic :: Software Development :: Object Brokering",
-        "Topic :: System :: Clustering",
-        "Topic :: System :: Distributed Computing",
-        "Topic :: System :: Monitoring",
-        "Topic :: System :: Networking",
-        "Topic :: System :: Systems Administration",
-        "Topic :: Scientific/Engineering :: Physics",
-    ],
-)
+setup()
